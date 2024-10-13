2a72cc184834e422e713a075d0a828c432592fde
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-13 19:17:25 +0000

    you

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function floor which takes a float n"""
+import math
+
+
+def floor(n: float) -> int:
+    """function floor which takes a float n as argument"""
+    return math.floor(n)
