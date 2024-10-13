debd1338d9436e5fc0ca3e6869480cdd1fcaf4fc
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-13 19:12:59 +0000

    you

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function concat that takes a string str1 and a string str2"""
+    return str1 + str2
