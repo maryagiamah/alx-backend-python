dae0e7571b5e0bce6002a177389b3c7f8aaf2f36
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-13 19:39:14 +0000

    you

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function to_str that takes a float n"""
+
+
+def to_str(n: float) -> str:
+    """function to_str that takes a float n as argument"""
+    return str(n)
