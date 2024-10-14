e00a2161c1b0583de39f0b549a95a8fc43f86ed0
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-14 14:00:39 +0000

    you

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay: int = 10)-> float:
+async def wait_random(max_delay: int = 10) -> float:
     """wait for a random delay between 0 and max_delay"""
     x = uniform(0, max_delay)
     await asyncio.sleep(x)
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" write an async routine called wait_n that takes in 2 ints"""
+form typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int)-> List[float]:
+    """"""
