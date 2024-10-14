6f0c6f0d7b0922c99813ad86dffc2c5eb0251148
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-14 13:19:46 +0000

    you

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine that takes in an integer argument"""
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10)-> float:
+    """wait for a random delay between 0 and max_delay"""
+    x = uniform(0, max_delay)
+    await asyncio.sleep(x)
+    return x
