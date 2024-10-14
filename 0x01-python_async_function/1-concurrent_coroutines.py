ac4810f1785f8ef59d8a417f35235708eb6ff08a
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-14 14:27:18 +0000

    you

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """ write an async routine called wait_n that takes in 2 ints"""
-form typing import List
+from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int)-> List[float]:
-    """"""
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """async routine called wait_n that takes in 2 int arguments"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*tasks)
+    return result
