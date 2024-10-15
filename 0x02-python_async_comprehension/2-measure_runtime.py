740f1f2c9eef85069fc49ef690a1b3c1b5709818
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-15 11:39:09 +0000

    you

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Import async_comprehension from the previous file"""
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+import time
+import asyncio
+
+
+async def measure_runtime() -> float:
+    """write a measure_runtime coroutine"""
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    start_time = time.perf_counter()
+    await asyncio.gather(*tasks)
+    return time.perf_counter() - start_time
