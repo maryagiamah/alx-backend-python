295bce1116053574f8097080d26a850f4de3ed61
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-15 11:40:22 +0000

    you

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Import async_comprehension from the previous file"""
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 import time
 import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
