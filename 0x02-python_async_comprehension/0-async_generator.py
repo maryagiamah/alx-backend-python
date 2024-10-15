6d8b1374c62c7d8153b28bef86b2d72da85f7854
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-15 10:50:44 +0000

    you

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Write a coroutine called async_generator that takes no arguments"""
+import random
+
+
+async def async_generator() -> int:
+    for i in range(10):
+        yeild i
