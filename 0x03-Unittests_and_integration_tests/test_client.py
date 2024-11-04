d2870e536b75f124828164c9e4fa417f62dfdd3f
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-04 12:25:49 +0100

    you

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,8 +34,9 @@ class TestGithubOrgClient(TestCase):
     def test_public_repos_url(self):
         """make it return a known payload"""
 
-        with patch(
-                'client.GithubOrgClient._public_repos_url',
+        with patch.object(
+                GithubOrgClient,
+                '_public_repos_url',
                 new_callable=PropertyMock) as mck:
             org_client = GithubOrgClient('google')
             mck.return_value = {"License": None, "name": "test_url"}
