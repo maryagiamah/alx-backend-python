2873311ce039f60947acce261ec5f340dd13bd07
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-04 12:11:56 +0100

    you

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -12,11 +12,10 @@ class TestGithubOrgClient(TestCase):
         ('google',),
         ('abc',)
     ])
-    @patch('client.get_json')  # Patch the get_json function
+    @patch('client.get_json')
     def test_org(self, org_name, mock_json):
         """Test that GithubOrgClient.org returns the correct value"""
 
-        # Define what the mock should return, mimicking the API response
         val = {
             'login': org_name,
             'repos_url': f'https://api.github.com/orgs/{org_name}/repos'
@@ -31,3 +30,17 @@ class TestGithubOrgClient(TestCase):
         mock_json.assert_called_once_with(
             org_client.ORG_URL.format(org=org_name)
         )
+
+    def test_public_repos_url(self):
+        """make it return a known payload"""
+
+        with patch.object(
+                GithubOrgClient,
+                '_public_repos_url',
+                new_callable=PropertyMock) as mck:
+            org_client = GithubOrgClient('google')
+            mck.return_value = {"License": None, "name": "test_url"}
+            self.assertEqual(
+                    org_client._public_repos_url,
+                    {"License": None, "name": "test_url"}
+                )
